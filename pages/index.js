3c3c2475c8f391e2a978c0a06c09b90854852777
numol1992 <numol1992@gmail.com>
2024-08-21 21:42:24 +0700

    add datadog-ci

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import { datadogRum } from '@datadog/browser-rum';
+
+datadogRum.init({
+    applicationId: '0e659b80-1f94-4961-8c8a-530812da595e',
+    clientToken: 'pubc8dde073ac286363ec0f2e8c5ed02a43',
+    // `site` refers to the Datadog site parameter of your organization
+    // see https://docs.datadoghq.com/getting_started/site/
+    site: 'datadoghq.com',
+    service: 'flutter_web_nm',
+    env: 'dev-nm',
+    // Specify a version number to identify the deployed version of your application in Datadog
+    version: '1.0.0',
+    sessionSampleRate: 100,
+    sessionReplaySampleRate: 20,
+    trackUserInteractions: true,
+    trackResources: true,
+    trackLongTasks: true,
+    defaultPrivacyLevel: 'mask-user-input',
+});
+
 
 export default function Home() {
   return (
